12c86480a49f4aa6f491356204245deeb5af43e5
evantwidwell <evanski@gmail.com>
2023-04-21 09:11:02 -0500

    31

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,70 @@
 # report.py
-#
-# Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            stock = {
+                 'name'   : row[0],
+                 'shares' : int(row[1]),
+                 'price'   : float(row[2])
+            }
+            portfolio.append(stock)
+
+    return portfolio
+
+
+def read_prices(filename):
+    '''
+    Read a CSV file of price data into a dict mapping names to prices.
+    '''
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+    return prices
+
+
+portfolio = read_portfolio('./Data/portfolio.csv')
+prices = read_prices('./Data/prices.csv')
+
+
+# Calculate the total cost of the portfolio
+def total_cost(p):
+    total_cost = 0.0
+    for s in p:
+        total_cost += s['shares']*s['price']
+    return total_cost
+
+
+# Compute the current value of the portfolio
+def current_value(p, cost):
+    total_value = 0.0
+    for s in p:
+        total_value += s['shares']*prices[s['name']]
+
+    print('Current value', total_value)
+    print('Gain', total_value - cost)
+    
+
+def print_report(port):
+    tcost = total_cost(port)
+    print('Total cost', tcost)
+    current_value(port, tcost)
+
+
+
